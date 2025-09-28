9a98c3f4285f25bbaef049f748ec8efbbaf06509
prgoc <tolfs14@gmail.com>
2025-09-28 15:48:02 +0000

    modified

--- tools/helper.hpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-#include <regex>
-
-std::vector < std::string > split(const std::string& str, const std::regex& delim);
-void print_token(const Token& token, std::string comment = "");
-void show_token(const Token& token);
-void show_line(const std::vector < Token >& row);
-void show(const std::vector < std::vector < Token > >& asts);
