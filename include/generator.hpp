3a539061758261ed54c168d667ac164f3a427421
prgoc <tolfs14@gmail.com>
2025-10-01 17:27:10 +0900

    modified spell

--- include/generator.hpp
+++ include/generator.hpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include "token.hpp"
 
-std::string generateHTMLStrimg(std::vector < Token >& tokens);
+std::string generateHTMLString(std::vector < Token >& tokens);
 std::string generate(std::vector < std::vector < Token > > asts);
 bool isAllElmParentRoot(std::vector < Token > tokens);
